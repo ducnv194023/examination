8027fb8757432811ff065c108a243367092f73a7
duc-san <duc.nv194023@sis.hust.edu.vn>
2024-05-21 17:27:59 +0700

    update

--- contracts/Examination.sol
+++ contracts/Examination.sol
@@ -3,20 +3,30 @@ pragma solidity ^0.8.24;
 
 contract Examination {
     struct Exam {
-        uint examId;
+        string examArweaveIdHash;
         bytes32[] hashQuestions;
         bytes32[] hashAnswers;
         string[] revealAnswers;
+        uint score;
     }
 
     Exam[] public exams;
 
     mapping(address => uint[]) public ownerExams;
 
-    function submitQuestion(bytes32[] memory _hashQuestions, uint _examId) public {
+    event ExamCreated(uint examId);
+    event PassedExam(address indexed answerer, uint examId);
+
+    function createExam(string memory _examArweaveIdHash) public {
         Exam storage newExam = exams.push();
-        newExam.examId = _examId;
-        newExam.hashQuestions = _hashQuestions;
+
+        newExam.examArweaveIdHash = _examArweaveIdHash;
+
+        emit ExamCreated(exams.length - 1);
+    }
+
+    function submitQuestion(bytes32[] memory _hashQuestions, uint _examId) public {
+        exams[_examId].hashQuestions = _hashQuestions;
     }
 
 
@@ -44,15 +54,20 @@ contract Examination {
         return false;
     }
 
-    function verifyCorrectAnswer(string memory answerSalt, uint _examId) view public {
+    function verifyCorrectAnswer(string memory answerSalt, uint _examId, uint targetScore) public {
         Exam memory exam = exams[_examId];
-
+        uint correctAnswers = 0;
         require(isOwnerExam(msg.sender, _examId), "You are not allowed to reveal the answer for this question.");
         for (uint i = 0; i < exam.hashAnswers.length; i++) {
-            require(keccak256(abi.encodePacked(exam.revealAnswers[i], answerSalt)) == exam.hashAnswers[i], "The answer is not correct.");
-            // TODO: Implement the logic to reward the user
+            if(keccak256(abi.encodePacked(exam.revealAnswers[i], answerSalt)) == exam.hashAnswers[i]) {
+                correctAnswers++;
+            }
         }
 
+        exam.score = correctAnswers;
 
+        if (exam.score >= targetScore) {
+            emit PassedExam(msg.sender, _examId);
+        }
     }
 }
